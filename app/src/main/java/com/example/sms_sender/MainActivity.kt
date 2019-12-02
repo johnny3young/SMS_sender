5531020ace61061a60dd1131e0aef20261b1626a
Johnny 3 Young <youngerblack@gmail.com>
2019-12-02 12:19:26 -0500

    Added annotation in the 3 input text (name, number and text of the message)

--- app/src/main/java/com/example/sms_sender/MainActivity.kt
+++ app/src/main/java/com/example/sms_sender/MainActivity.kt
@@ -19,26 +19,51 @@ class MainActivity : AppCompatActivity() {
 
         btn_send_msj.setOnClickListener {
 
-            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.SEND_SMS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
 
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), requestSendSms)
-            }else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    requestSendSms
+                )
+            } else {
                 sendSms()
             }
+
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == requestSendSms) sendSms()
     }
 
     private fun sendSms() {
-        val number = txtview_number_send.text.toString()
-        val name = txtview_name_send
-        val mensaje = editxt_msj_to_send.text.toString()
+        val number = edit_text_number_send.text.toString()
+        if (number == "") {
+            Toast.makeText(this, "Digite un número de celular válido", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val name = edit_text_name_send.text.toString()
+        if (name == "") {
+            Toast.makeText(this, "Nombre del destinatario vacío", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val mensaje = edit_text_msj_to_send.text.toString()
+        if (mensaje == "") {
+            Toast.makeText(this, "Escriba el mensaje a enviar", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-        SmsManager.getDefault().sendTextMessage(number,null,mensaje,null,null)
-        Toast.makeText(applicationContext,"Mensaje enviado",Toast.LENGTH_SHORT).show()
+        SmsManager.getDefault().sendTextMessage(number, null, mensaje, null, null)
+        Toast.makeText(applicationContext, "Mensaje enviado", Toast.LENGTH_SHORT).show()
     }
 
 
