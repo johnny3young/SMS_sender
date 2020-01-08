53e74ad8a9272ec927185573cf31f159ef269d87
Black3 <youngerblack@gmail.com>
2020-01-08 13:13:48 -0500

    Default permission added, missing default controller for app

--- app/src/main/java/com/example/sms_sender/MainActivity.kt
+++ app/src/main/java/com/example/sms_sender/MainActivity.kt
@@ -1,48 +1,166 @@
 package com.example.sms_sender
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.provider.Telephony
 import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.telephony.SmsManager
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+import java.util.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    private val requestSendSms: Int = 2
+
+    companion object{
+
+        const val REQUEST_SMS_PERMISSIONS: Int = 2
+        const val REQUEST_ID_MULTIPLE_PERMISSIONS: Int = 1
+        private val  SPLASH_TIME_OUT = 2000
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         btn_send_msj.setOnClickListener {
 
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.SEND_SMS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.SEND_SMS),
-                    requestSendSms
-                )
-            } else {
+            if (checkAndRequestPermissions()){
+                val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+                setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
+                startActivityForResult(setSmsAppIntent, REQUEST_ID_MULTIPLE_PERMISSIONS)
                 sendSms()
             }
 
+
         }
+
+    }
+    private fun checkAndRequestPermissions(): Boolean {
+        val camerapermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val writepermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        val permissionLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        val permissionRecordAudio = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+        val permissionSms = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+
+
+        val listPermissionsNeeded = ArrayList<String>()
+
+        if (camerapermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA)
+        }
+        if (writepermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        if (permissionRecordAudio != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.RECORD_AUDIO)
+        }
+        if (permissionSms != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.SEND_SMS)
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toTypedArray(), REQUEST_ID_MULTIPLE_PERMISSIONS)
+            return false
+        }
+        return true
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == requestSendSms) sendSms()
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        Log.d("TAG", "Permission callback called-------")
+        when (requestCode) {
+            REQUEST_ID_MULTIPLE_PERMISSIONS -> {
+
+                val perms = HashMap<String, Int>()
+
+                // Initialize the map with both permissions
+                perms[Manifest.permission.CAMERA] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.ACCESS_FINE_LOCATION] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.RECORD_AUDIO] = PackageManager.PERMISSION_GRANTED
+                perms[Manifest.permission.SEND_SMS] = PackageManager.PERMISSION_GRANTED
+
+                // Fill with actual results from user
+                if (grantResults.size > 0) {
+                    for (i in permissions.indices)
+                        perms[permissions[i]] = grantResults[i]
+
+                    // Check for both permissions
+                    if (perms[Manifest.permission.CAMERA] == PackageManager.PERMISSION_GRANTED
+                        && perms[Manifest.permission.WRITE_EXTERNAL_STORAGE] == PackageManager.PERMISSION_GRANTED
+                        && perms[Manifest.permission.ACCESS_FINE_LOCATION] == PackageManager.PERMISSION_GRANTED
+                        && perms[Manifest.permission.RECORD_AUDIO] == PackageManager.PERMISSION_GRANTED
+                        && perms[Manifest.permission.SEND_SMS] == PackageManager.PERMISSION_GRANTED)
+                    {
+                        Log.d("TAG", "sms & location services permission granted")
+
+                        //else any one or both the permissions are not granted
+                    } else {
+                        Log.d("TAG", "Some permissions are not granted ask again ")
+                        //permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
+                        //                        // shouldShowRequestPermissionRationale will return true
+                        //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS))
+                        {
+                            showDialogOK("Service Permissions are required for this app",
+                                DialogInterface.OnClickListener { dialog, which ->
+                                    when (which) {
+                                        DialogInterface.BUTTON_POSITIVE -> checkAndRequestPermissions()
+                                        DialogInterface.BUTTON_NEGATIVE -> return@OnClickListener
+
+                                    }
+                                })
+                        } else {
+                            explain("You need to give some mandatory permissions to continue. Do you want to go to app settings?")
+                            //                            //proceed with logic by disabling the related features or quit the app.
+                        }//permission is denied (and never ask again is  checked)
+                        //shouldShowRequestPermissionRationale will return false
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun showDialogOK(message: String, okListener: DialogInterface.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton("OK", okListener)
+            .setNegativeButton("Cancel", okListener)
+            .create()
+            .show()
+    }
+
+    private fun explain(msg: String) {
+        val dialog = android.support.v7.app.AlertDialog.Builder(this)
+        dialog.setMessage(msg)
+            .setPositiveButton("Yes") { paramDialogInterface, paramInt ->
+                val uri = Uri.fromParts("package", getPackageName(), null)
+                startActivity(Intent(ACTION_APPLICATION_DETAILS_SETTINGS, uri))
+
+            }
+            .setNegativeButton("Cancel") { paramDialogInterface, paramInt -> return@setNegativeButton }
+        dialog.show()
     }
 
     private fun sendSms() {
@@ -65,6 +183,5 @@ class MainActivity : AppCompatActivity() {
         SmsManager.getDefault().sendTextMessage(number, null, mensaje, null, null)
         Toast.makeText(applicationContext, "Mensaje enviado", Toast.LENGTH_SHORT).show()
     }
-
 
 }
